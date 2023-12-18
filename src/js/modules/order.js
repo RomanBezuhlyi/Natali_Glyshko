798e65206f20465c3734412a82ad3b396be2ae44
Roman <bezuglijj7@gmail.com>
2023-12-18 18:18:47 +0200

    fixed bugs and added map

--- src/js/modules/order.js
+++ src/js/modules/order.js
@@ -26,14 +26,34 @@ var processTopSwiper = new Swiper(".order__price-row", {
 // Отримати всі елементи послуг
 const serviceItems = document.querySelectorAll(".item");
 
-// Отримати елемент span у формі, де ви хочете відображати обрану послугу
+// Отримати елемент span у формі, де ви хочете відображати обрані послуги
 const serviceSpan = document.querySelector(".order__form-service span");
+
+// Зберігати вибрані послуги в масиві
+const selectedServices = [];
 
 // Додати слухача подій click до кожної карти послуги
 serviceItems.forEach((item) => {
   item.addEventListener("click", () => {
-    // Оновити вміст спану обраною назвою послуги
-    serviceSpan.textContent = item.querySelector(".item__name").textContent;
+    // Отримати назву обраної послуги
+    const serviceName = item.querySelector(".item__name").textContent;
+
+    // Перевірити, чи ця послуга вже обрана
+    const isSelected = selectedServices.includes(serviceName);
+
+    if (!isSelected) {
+      // Якщо послуга ще не обрана, додати її до масиву обраних послуг
+      selectedServices.push(serviceName);
+    } else {
+      // Якщо послуга вже обрана, видалити її з масиву обраних послуг
+      const index = selectedServices.indexOf(serviceName);
+      if (index !== -1) {
+        selectedServices.splice(index, 1);
+      }
+    }
+
+    // Оновити вміст спану і відобразити обрані послуги через "+"
+    serviceSpan.textContent = selectedServices.join(" + ");
   });
 });
 
@@ -50,4 +70,25 @@ callButton.addEventListener("click", function () {
 
   // З'явлення посилання
   numberLink.style.display = "flex"; // або 'block', залежно від вашого дизайну
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".item");
+  const modalOverlay = document.querySelector(".modal-overlay-second");
+  const modalFormService = document.querySelector(".modal__form-service span");
+  const modalCloseBtn = document.querySelector(".modal__close-second");
+
+  items.forEach((item) => {
+    item.addEventListener("click", function () {
+      if (innerWidth <= 991) {
+        const serviceName = item.querySelector(".item__name").innerText;
+        modalFormService.innerText = serviceName;
+        modalOverlay.style.display = "flex";
+      }
+    });
+  });
+
+  modalCloseBtn.addEventListener("click", function () {
+    modalOverlay.style.display = "none";
+  });
 });
